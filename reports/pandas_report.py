b4b44e394b0d3ec2fe57b5a4b58aa73d7e5734b9
prlz77 <pau.rodri1@gmail.com>
2018-06-28 19:17:00 +0200

    allow choosing x axis

--- reports/pandas_report.py
+++ reports/pandas_report.py
@@ -50,33 +50,33 @@ def read_path(path_lst, pre_fn=lambda x: x, filter=None):
     return logs
 
 
-def report(logs, target_field, output, avg):
+def report(logs, target_field, output, avg, x_axis):
     idx = logs[target_field].replace(np.NaN, -1).groupby(level=0).idxmax()
     ret = pd.DataFrame(logs, index=idx)
     if avg:
         ret = ret.applymap(lambda x: str(x) if isinstance(x, list) else x)
         columns = list(ret.columns)
         columns.remove(target_field)
-        columns.remove("epoch")
+        columns.remove(x_axis)
         ret = ret.groupby(columns).mean()
 
     ret.to_csv(output)
 
 
-def plot_pandas(logs, target_field, columns):
+def plot_pandas(logs, target_field, columns, x_axis):
     columns = logs.keys() if columns is None else columns
-    columns = list(set(columns) - set([target_field, "epoch"]))
-    ret = logs.pivot_table(index="epoch", columns=columns, values=target_field)
+    columns = list(set(columns) - set([target_field, x_axis]))
+    ret = logs.pivot_table(index=x_axis, columns=columns, values=target_field)
     ret.plot()
     pylab.gca().get_legend().draggable()
     pylab.show()
 
-def plot_confidence(logs, target_field, columns):
+def plot_confidence(logs, target_field, columns, x_axis):
     columns = logs.keys() if columns is None else columns
-    columns = list(set(columns) - set([target_field, "epoch"]))
+    columns = list(set(columns) - set([target_field, x_axis]))
     logs = logs.applymap(lambda x: str(x) if isinstance(x, list) else x)
    # logs = pd.DataFrame(logs, index=logs[target_field].unstack().dropna(how='all').stack().index)
-    ret = logs.pivot_table(index="epoch", columns=columns, values=target_field, aggfunc=[np.nanmin, np.nanmax, np.nanmean])
+    ret = logs.pivot_table(index=x_axis, columns=columns, values=target_field, aggfunc=[np.nanmin, np.nanmax, np.nanmean])
     ret_dict = ret.to_dict('list')
     new_dict = {}
     max_values = []
@@ -101,7 +101,7 @@ def plot_confidence(logs, target_field, columns):
         mean = data['nanmean']
         pylab.fill_between(range(len(amin)), amin, amax, alpha=0.5, color=colors[i], )
         pylab.plot(range(len(amin)), mean, color=colors[i], linestyle=styles[i % len(styles)])
-    pylab.xlabel("epoch")
+    pylab.xlabel(x_axis)
     pylab.ylabel(target_field)
     pylab.title("%s: %s" %(target_field, str(columns)))
     keys = [",".join([str(x) for x in max_keys[i]]) for i in idx]
@@ -128,19 +128,19 @@ def main(args):
     if args.columns is not None:
         columns = set(args.columns)
         columns.add(args.target_field)
-        columns.add("epoch")
+        columns.add(args.x_axis)
         columns = list(columns)
     else:
         columns = None
     logs = read_path(args.paths, lambda x: load_fn(x, columns, args.filter_all), args.filter_column)
     logs = pd.concat(logs, keys=range(len(logs)))
     if args.command == "s" or args.command == "summarize":
-        report(logs, args.target_field, args.summary_output, args.avg)
+        report(logs, args.target_field, args.summary_output, args.avg, args.x_axis)
     elif args.command == "p" or args.command == "plot":
         if args.confidence:
-            plot_confidence(logs, args.target_field, columns)
+            plot_confidence(logs, args.target_field, columns, args.x_axis)
         else:
-            plot_pandas(logs, args.target_field, columns)
+            plot_pandas(logs, args.target_field, columns, args.x_axis)
 
 
 if __name__ == "__main__":
@@ -151,6 +151,7 @@ if __name__ == "__main__":
     parser.add_argument("--columns", "-c", type=str, nargs="*", default=None)
     parser.add_argument("--filter_column", "-f", type=str, nargs="?", default=None, help="$depth > 16")
     parser.add_argument("--filter_all", type=str, default="", help="$accuracy > 0")
+    parser.add_argument("--x_axis", type=str, default="epoch")
     # Summary options
     parser.add_argument("--summary_output", "-so", type=str, default="report.csv")
     parser.add_argument("--avg", action="store_true")
